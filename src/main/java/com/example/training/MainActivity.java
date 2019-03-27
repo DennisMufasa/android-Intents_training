eff98759d1608e703c3e6497b143b9ab9ffe8967
DennisMufasa <denny.muasa@gmail.com>
2019-03-27 22:22:13 +0300

    place a stroy on a scrolling activity

--- src/main/java/com/example/training/MainActivity.java
+++ src/main/java/com/example/training/MainActivity.java
@@ -4,14 +4,13 @@ package com.example.training;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Display;
 import android.view.View;
 import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnSend;
+    Button btnStory, btnVideo;
 
 
     @Override
@@ -25,12 +24,22 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void getDisplay(){
+        btnStory = (Button)findViewById(R.id.btnStory);
+        btnVideo = (Button)findViewById(R.id.btnVideo);
 
-        btnSend = (Button)findViewById(R.id.btnSend);
-        btnSend.setOnClickListener(new View.OnClickListener() {
+
+        btnVideo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), display.class);
+                startActivity(intent);
+            }
+        });
+
+        btnStory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), story.class);
                 startActivity(intent);
             }
         });
